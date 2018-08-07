58de06dc22eb23d00b1ac0b4204890ac22ac7fb1
macyan2000 <39038146+macyan2000@users.noreply.github.com>
2018-08-07 11:38:55 +0800

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -12,7 +12,6 @@ extern "C" {
     #include "scryptn.h"
     #include "yescrypt/yescrypt.h"
     #include "yescrypt/sha256.h"
-    #include "yescrypt/sha256_Y.h"
     #include "skein.h"
     #include "x11.h"
     #include "groestl.h"
