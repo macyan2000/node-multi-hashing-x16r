8c9fe2bf3bb71630a3b422917dfc3f188b9b5104
macyan2000 <39038146+macyan2000@users.noreply.github.com>
2018-09-12 16:08:44 +0800

    test

--- multihashing.cc
+++ multihashing.cc
@@ -214,8 +214,7 @@ void yespower(const FunctionCallbackInfo<Value>& args) {
    char * input = Buffer::Data(target);
    char* output = new char[32];
 
-   
-   yespower_hash(input, output, input_len);
+   yespower_hash(input, output;
 
    Local<Object> buff = Nan::NewBuffer(output, 32).ToLocalChecked();
    args.GetReturnValue().Set(buff);
